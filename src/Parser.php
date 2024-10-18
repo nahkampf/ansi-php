9a31ca9d8c850973e0a776ee88c9b21514a8ca32
Sebastian Utbult <kreuzweg@gmail.com>
2024-10-18 12:38:32 +0200

    Support for initializing Parser in ANSIBBS compatible mode (#2)
    * A little fix for support oldschool SGR color mode (bold bit on/off)
    in the parser
    
    * Also fix setting the high intensity bit to low on lowintensity colors

--- src/Parser.php
+++ src/Parser.php
@@ -19,6 +19,13 @@
  *
  * The *correct* usage would of course be to only allow colors 0-7 as background colors
  * LIKE NATURE INTENDED but we're not going to be that opinionated in code.
+ *
+ * NOTE that Parser can be instantiated in two modes: AIXTERM or ANSIBBS. The AIXTERM mode
+ * makes use of more modern escape sequences to render bright colors (\e[90m) while the
+ * ANSIBBS mode uses a combination of the 8 "low intensity" colors and a bit set for "bold"/
+ * "high intensity" (like \e[1;31m). If you're building for modern terminals you are probably
+ * fine with default AIXTERM mode, but if you're building for something using an older
+ * ANSI capable console (ANSI.SYS, a BBS door or similar) you want ANSIBBS mode.
  */
 
 namespace Bramus\Ansi;
@@ -32,7 +39,32 @@ class Parser
      * @var Ansi
      */
     protected $ansi;
-    private $fgColorLookup = [
+    /**
+     * What SGR mode to use (AIXTERM or ANSIBBS)
+     * @var int
+     */
+    protected $mode;
+    const MODE_AIXTERM = 0;
+    const MODE_ANSIBBS = 1;
+    private $fgColorLookupANSIBBS = [
+        0 => "0;" . SGR::COLOR_FG_BLACK,
+        1 => "0;" . SGR::COLOR_FG_BLUE,
+        2 => "0;" . SGR::COLOR_FG_GREEN,
+        3 => "0;" . SGR::COLOR_FG_CYAN,
+        4 => "0;" . SGR::COLOR_FG_RED,
+        5 => "0;" . SGR::COLOR_FG_PURPLE,
+        6 => "0;" . SGR::COLOR_FG_YELLOW,
+        7 => "0;" . SGR::COLOR_FG_WHITE,
+        8 => "1;" . SGR::COLOR_FG_BLACK,
+        9 => "1;" . SGR::COLOR_FG_BLUE,
+        10 => "1;" . SGR::COLOR_FG_GREEN,
+        11 => "1;" . SGR::COLOR_FG_CYAN,
+        12 => "1;" . SGR::COLOR_FG_RED,
+        13 => "1;" . SGR::COLOR_FG_PURPLE,
+        14 => "1;" . SGR::COLOR_FG_YELLOW,
+        15 => "1;" . SGR::COLOR_FG_WHITE
+    ];
+    private $fgColorLookupDefault = [
         0 => SGR::COLOR_FG_BLACK,
         1 => SGR::COLOR_FG_BLUE,
         2 => SGR::COLOR_FG_GREEN,
@@ -48,7 +80,7 @@ class Parser
         12 => SGR::COLOR_FG_RED_BRIGHT,
         13 => SGR::COLOR_FG_PURPLE_BRIGHT,
         14 => SGR::COLOR_FG_YELLOW_BRIGHT,
-        15 => SGR::COLOR_FG_WHITE_BRIGHT,
+        15 => SGR::COLOR_FG_WHITE_BRIGHT
     ];
     private $bgColorLookup = [
         0 => SGR::COLOR_BG_BLACK,
@@ -68,9 +100,10 @@ class Parser
         14 => SGR::COLOR_BG_YELLOW_BRIGHT,
         15 => SGR::COLOR_BG_WHITE_BRIGHT,
     ];
-    public function __construct($ansi)
+    public function __construct($ansi, $mode = self::MODE_AIXTERM)
     {
         $this->ansi = $ansi;
+        $this->mode = $mode;
     }
 
     public function parse($string): void
@@ -113,7 +146,11 @@ class Parser
                 continue;
             }
             if (preg_match("/^f\d{1,2}/i", $chunk)) {
-                $this->ansi->color($this->fgColorLookup[substr($chunk, 1)]);
+                if ($this->mode == self::MODE_AIXTERM) {
+                    $this->ansi->color($this->fgColorLookupDefault[substr($chunk, 1)]);
+                } else {
+                    $this->ansi->color($this->fgColorLookupANSIBBS[substr($chunk, 1)]);
+                }
                 continue;
             }
             if (preg_match("/^b\d{1,2}/i", $chunk)) {
